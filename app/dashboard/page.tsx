77b3e7695546c634b1e81465fc42838f56b69b71
songjilong <songjilong@threebody.xyz>
2024-02-07 15:12:52 +0800

    update: 删除无用代码

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card";
+import './page.css'
 import UserInfo from "./components/use-info";
 import Motto from "./components/motto";
 import Signature from "./components/signature";
@@ -6,7 +7,7 @@ import Date from "./components/date";
 export default function Page() {
   return (
     <div>
-      <section style={{ height: "calc(100vh - 100px)" }} className="h-screen py-10 mx-auto max-w-7xl px-12 sm:px-10 lg:px-24 grid grid-rows-3 grid-cols-4 gap-4 grid-flow-row auto-rows-50">
+      <section style={{ height: "calc(100vh - 100px)" }} className="transform-3d h-screen py-10 mx-auto max-w-7xl px-12 sm:px-10 lg:px-24 grid grid-rows-3 grid-cols-4 gap-4 grid-flow-row auto-rows-50">
         <Card className="row-span-2 col-span-1">
           <CardContent>
             {/* <UserInfo></UserInfo> */}
@@ -17,7 +18,7 @@ export default function Page() {
             {/* <UserInfo></UserInfo> */}
           </CardContent>
         </Card>
-        <Card className="flex items-center justify-center row-span-1 col-span-1">
+        <Card className="transform-3d-item flex items-center justify-center row-span-1 col-span-1 shadow-lg transform hover:scale-105 transition duration-300 ease-in-out">
           <CardContent>
             <Signature></Signature>
           </CardContent>
