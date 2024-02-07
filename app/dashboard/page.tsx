612039f1050e7c724f75213178b2db186d123bdd
songjilong <songjilong@threebody.xyz>
2024-02-07 11:18:04 +0800

    update: home page to bento layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,32 +6,40 @@ import Date from "./components/date";
 export default function Page() {
   return (
     <div>
-      <section className="mx-auto max-w-7xl px-12 sm:px-10 lg:px-24 grid grid-rows-3 grid-flow-col gap-4 mt-8 h-60">
-        <Card className="row-span-3 ...">
+      <section style={{ height: "calc(100vh - 100px)" }} className="h-screen py-10 mx-auto max-w-7xl px-12 sm:px-10 lg:px-24 grid grid-rows-3 grid-cols-4 gap-4 grid-flow-row auto-rows-50">
+        <Card className="row-span-2 col-span-1">
+          <CardContent>
+            {/* <UserInfo></UserInfo> */}
+          </CardContent>
+        </Card>
+        <Card className="row-span-1 col-span-2 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% ...">
+          <CardContent>
+            {/* <UserInfo></UserInfo> */}
+          </CardContent>
+        </Card>
+        <Card className="flex items-center justify-center row-span-1 col-span-1">
+          <CardContent>
+            <Signature></Signature>
+          </CardContent>
+        </Card>
+        <Card className="row-span-1 col-span-2">
           <CardContent>
             <UserInfo></UserInfo>
           </CardContent>
         </Card>
-        <Card className="flex justify-center items-center col-span-2 ...">
+        <Card className="row-span-2 col-span-1">
           <CardContent>
-            <Motto></Motto>
+            {/* <UserInfo></UserInfo> */}
           </CardContent>
         </Card>
-        <Card className="flex justify-center items-center row-span-2 col-span-1 ...">
+        <Card className="row-span-1 col-span-2">
           <CardContent>
-            <Signature></Signature>
+            {/* <UserInfo></UserInfo> */}
           </CardContent>
         </Card>
-        <Card className="flex justify-center items-center row-span-2 col-span-1 ...">
+        <Card className="row-span-1 col-span-1">
           <CardContent>
-            <Signature></Signature>
-          </CardContent>
-        </Card>
-      </section>
-      <section className="mx-auto max-w-7xl px-12 sm:px-10 lg:px-24 grid grid-rows-3 grid-flow-col gap-4 mt-8 h-60">
-        <Card className="row-span-4 col-span-1">
-          <CardContent>
-            <Date></Date>
+            {/* <UserInfo></UserInfo> */}
           </CardContent>
         </Card>
       </section>
