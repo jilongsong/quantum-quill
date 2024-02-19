af4ee823324ce2e395de90bc8d9fc0f687284717
songjilong <songjilong@threebody.xyz>
2024-02-19 22:30:35 +0800

    update: blog-detail layout

--- app/blog/detail/page.tsx
+++ app/blog/detail/page.tsx
@@ -22,9 +22,11 @@ export default function Page() {
   fetchPostDetail;
 
   return (
-    <div className="min-h-full mt-4" style={{ height: "calc(100vh - 100px)" }}>
-      <div className="h-full mx-auto max-w-7xl px-6 sm:px-2 overflow-y-scroll">
-      <Markdown content={blogDetail?.content} />
+    <div className="min-h-full mt-4">
+      <div className="flex flex-row h-full mx-auto max-w-7xl px-6 sm:px-2">
+      <div className="basis-1/5 text-center">左侧导航</div>
+      <div className="basis-3/5 overflow-y-scroll" style={{ height: "calc(100vh - 100px)" }}><Markdown content={blogDetail?.content} /></div>
+      <div className="basis-1/5 text-center">右侧工具</div>
       </div>
     </div>
   );
